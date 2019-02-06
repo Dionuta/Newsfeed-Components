5708ae5124ad30655f8c24d397c129dda3ac3076
Dionuta <diounta@gmail.com>
2019-02-05 19:58:44 -0800

    Mvp

--- Article/Article.js
+++ Article/Article.js
@@ -1,19 +1,20 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(newArticel) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = newArticel;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = newArticel.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent ='expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click',  () => { this.expandArticle() });
   }
 
-  expandArticle() {
+  expandArticle () {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle('article-open');
 
   }
 }
@@ -26,4 +27,7 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+//loop over new NodeList (articles) genrate new Article from stratch
+articles.forEach(articel => new Article (articel));
